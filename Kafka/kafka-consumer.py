64b4064b3a95fc190487adb5d168edba80876c1d
Ashwani Jindal <ashwani.07@outlook.com>
2019-07-07 21:32:48 +0530

    Minor changes

--- Kafka/kafka-consumer.py
+++ Kafka/kafka-consumer.py
@@ -1,13 +1,12 @@
-# Ref: https://blog.k2datascience.com/running-kafka-using-docker-332207aec73c
 from kafka import KafkaConsumer
 
 # continuous loop
-var = 1
-while var == 1:
+loop = 1
+while loop == 1:
 
     # initialize consumer to given topic and broker. IP is of kafka docker container.
     consumer = KafkaConsumer('test-topic', group_id='consumer-1', \
-        bootstrap_servers='172.19.0.3:9094')
+        bootstrap_servers='192.168.32.3:9094')
 
     # loop and print messages
     for msg in consumer:
--- Kafka/kafka-producer.py
+++ Kafka/kafka-producer.py
@@ -1,23 +1,27 @@
-# Ref: https://blog.k2datascience.com/running-kafka-using-docker-332207aec73c
 import time
 import random
+import string
 from kafka import KafkaProducer
 
+letters = string.ascii_lowercase
+
 # create producer. IP is of kafka docker container.
-producer = KafkaProducer(bootstrap_servers='172.19.0.3:9094')
+producer = KafkaProducer(bootstrap_servers='192.168.32.3:9094')
 
 # continuous loop
-var = 1
-while var == 1:
+loop = 1
+while loop == 1:
 
-    # generate a random integer
+    # generate a random number and word
     num = random.randint(0, 10)
-    
+    word = ''.join(random.choice(letters) for i in range(num))
+
     # message value and key must be raw bytes
     num_bytes = bytes(str(num), encoding='utf-8')
+    word_bytes = bytes(word, encoding='utf-8' )
 
     # send to topic on broker
-    producer.send('test-topic', value=num_bytes, key=num_bytes)
+    producer.send('test-topic', value=word_bytes, key=num_bytes)
 
     # wait 1 second
     time.sleep(1)
